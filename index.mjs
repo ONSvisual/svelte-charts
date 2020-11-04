871460b1a2d7f2fca9a11610448bddefefbbbd27
Ahmad Barclay <ahmadbarclay@Ahmads-MacBook-Pro.local>
2020-11-04 12:46:34 +0200

    Initial commit

--- index.mjs
+++ index.mjs
@@ -3,6 +3,6 @@ export { default as ScatterChart } from './components/ScatterChart.svelte';
 
 // Shared components
 export { default as AxisX } from './components/shared/AxisX.svelte';
-export { default as AxisX } from './components/shared/AxisY.svelte';
-export { default as AxisX } from './components/shared/QuadTree.svelte';
-export { default as AxisX } from './components/shared/Scatter.svelte';
+export { default as AxisY } from './components/shared/AxisY.svelte';
+export { default as QuadTree } from './components/shared/QuadTree.svelte';
+export { default as Scatter } from './components/shared/Scatter.svelte';
