1c074045874d00adc3f3af09c8ccae08bc53ab06
henryjameslau <henry.james.lau@gmail.com>
2021-01-22 11:19:08 +0000

    hmm

---
+++ charts/shared/Bubble.svelte
@@ -0,0 +1,20 @@
+<script>
+  import {
+    getContext
+  } from 'svelte';
+
+  const {
+    data,
+    xGet,
+    yGet,
+    xScale,
+    yScale
+  } = getContext('LayerCake');
+
+  export let fill = "#ccc"
+</script>
+<g class="bubble-group">
+  {#each $data as d,i}
+  <circle class='circle-bubble' , data-id="{i}" cx="{$xGet.(d)}" cy="{$yGet.(d)}" r="{$rGet(d)}" {fill}></circle>
+  {/each}
+</g>
