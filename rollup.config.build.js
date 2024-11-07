6f8b0de66432946b0651082a90bf45face92deea
Ahmad Barclay <ahmad.barclay@ons.gov.uk>
2024-11-07 12:45:11 +0000

    added class export for Chart

--- rollup.config.build.js
+++ rollup.config.build.js
@@ -7,7 +7,7 @@ import json from "@rollup/plugin-json";
 import css from 'rollup-plugin-css-only';
 
 // List of modules to compile (must match .js filenames in src, eg. LineChart.js => LineChart)
-const modules = ['LineChart', 'BarChart', 'ColumnChart', 'ScatterChart', 'DotPlotChart'];
+const modules = ['Chart', 'LineChart', 'BarChart', 'ColumnChart', 'ScatterChart', 'DotPlotChart'];
 
 export default [
 	...modules.map(module => {
